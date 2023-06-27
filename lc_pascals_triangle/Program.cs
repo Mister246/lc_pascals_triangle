51c2a113d6a04140fab86c61074e6b0b802157e6
Joel Elrod <joelelrod246@gmail.com>
2023-06-27 11:30:20 -0400

    Added basic solution template
    Signed-off-by: Joel Elrod <joelelrod246@gmail.com>

--- lc_pascals_triangle/Program.cs
+++ lc_pascals_triangle/Program.cs
@@ -1,11 +1,29 @@
 ﻿
 
 
+Solution.Generate(2);
+
 
 public class Solution
 {
-    public IList<IList<int>> Generate(int numRows)
+    static public IList<IList<int>> Generate(int numRows)
     {
+        IList<IList<int>> pascalsTriangle = new List<IList<int>>();
 
+        for (int i = 0; i < numRows; i++)
+        // for each row
+        {
+            IList<int> newRow = new List<int>();
+            newRow.Add(1);
+
+            // IF (i < 2)
+            //      init newRow with (i + 1) 1's
+            // ELSE
+            //      
+
+            pascalsTriangle[i] = newRow; // add new row to triangle
+        }
+
+        return pascalsTriangle;
     }
 }
